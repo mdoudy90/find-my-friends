8504e481ef651c606dd0e2b55173ca28abeca8dd
Michael Doudy <michael.doudy@gmail.com>
2020-07-31 13:32:24 -0400

    Update styles

--- client/src/components/PlaceMarker.jsx
+++ client/src/components/PlaceMarker.jsx
@@ -3,16 +3,14 @@ import React, { useState, useEffect } from 'react';
 const PlaceMarker = ({ text, isPlaceOfInterest }) => {
   const [highlighted, toggleHighlighted] = useState(false);
 
-  return <div className = 'place-marker'>
-      <div className = {isPlaceOfInterest ? 'placeOfInterest-marker-icon' : 'place-marker-icon'}
-      onClick={() => {
-        toggleHighlighted(!highlighted);
-        setTimeout(function(){ toggleHighlighted(!highlighted); }, 3000);
-        }
-      }
-      ></div>
-      <p style = { highlighted || isPlaceOfInterest ? { display: 'inline' } : { display: 'none' }} >{text}</p>
-    </div>;
+  return (
+    <div className='place-marker'>
+      <div
+        className={isPlaceOfInterest ? 'placeOfInterest-marker-icon' : 'place-marker-icon'}
+        onClick={() => toggleHighlighted(!highlighted)}></div>
+      <p style={highlighted || isPlaceOfInterest ? { display: 'inline' } : { display: 'none' }}>{text}</p>
+    </div>
+  );
 };
 
 export default PlaceMarker;
